8d72ff818397dc6fc5a843357ce0c597dc839897
josh <joshua.tee@gmail.com>
2019-05-03 21:08:19 -0400

    objectPolygonWarning work

--- wX/coreobjects/ObjectPolygonWarning.swift
+++ wX/coreobjects/ObjectPolygonWarning.swift
@@ -6,57 +6,58 @@
 
 class ObjectPolygonWarning {
 
-    //var storage: DataStorage
-    //var isEnabled = false;
-    //var type: PolygonType
+    var storage: DataStorage
+    var isEnabled = false;
+    var type: PolygonTypeGeneric
 
-    init(_ type: PolygonType) {
-        //isEnabled = Utility.readPref(prefTokenEnabled, "false").hasPrefix("t")
-        //storage = DataStorage(prefTokenStorage)
-        //storage.update()
+    init(_ type: PolygonTypeGeneric) {
+        self.type = type
+        isEnabled = Utility.readPref("RADAR_SHOW_\(type)", "false").hasPrefix("t")
+        storage = DataStorage("SEVERE_DASHBOARD_\(type)")
+        storage.update()
     }
 
-    /*var color: Int {return Utility.readPref(prefTokenColor, defaultColors[type])}
-    var name: String {return longName[type].replaceAll("%20", " ")}
+    var color: Int {return Utility.readPref(prefTokenColor, defaultColors[type]!)}
+    var name: String {return longName[type]!.replaceAll("%20", " ")}
     var prefTokenEnabled: String {return "RADAR_SHOW_" + typeName}
     var prefTokenColor: String {return "RADAR_COLOR_" + typeName}
     var prefTokenStorage: String {return "SEVERE_DASHBOARD_" + typeName}
-    var typeName: String {return type.string.replaceAll("PolygonType.", "")}
-    var urlToken: String {return longName[type]}
+    var typeName: String {return "\(type)".replaceAll("PolygonType.", "")}
+    var urlToken: String {return longName[type]!}
     var url: String {return baseUrl + urlToken}
     
-    static let defaultColors: [PolygonType: Int] = [
-        PolygonType.SMW: wXColor.colorsToInt(255, 165, 0),
-        PolygonType.SQW: wXColor.colorsToInt(199, 21, 133),
-        PolygonType.DSW: wXColor.colorsToInt(255, 228, 196),
-        PolygonType.SPS: wXColor.colorsToInt(255, 228, 181),
-        PolygonType.TOR: wXColor.colorsToInt(243, 85, 243),
-        PolygonType.TST: wXColor.colorsToInt(255, 255, 0),
-        PolygonType.FFW: wXColor.colorsToInt(0, 255, 0),
+    let defaultColors: [PolygonTypeGeneric: Int] = [
+        .SMW: wXColor.colorsToInt(255, 165, 0),
+        .SQW: wXColor.colorsToInt(199, 21, 133),
+        .DSW: wXColor.colorsToInt(255, 228, 196),
+        .SPS: wXColor.colorsToInt(255, 228, 181),
+        //PolygonType.TOR: wXColor.colorsToInt(243, 85, 243),
+        //PolygonType.TST: wXColor.colorsToInt(255, 255, 0),
+        //PolygonType.FFW: wXColor.colorsToInt(0, 255, 0),
         ]
     
-    static let longName: [PolygonType: String] = [
-        PolygonType.SMW: "Special%20Marine%20Warning",
-        PolygonType.SQW: "Snow%20Squall%20Warning",
-        PolygonType.DSW: "Dust%20Storm%20Warning",
-        PolygonType.SPS: "Special%20Weather%20Statement",
-        PolygonType.TOR: "Tornado%20Warning",
-        PolygonType.TST: "Severe%20Thunderstorm%20Warning",
-        PolygonType.FFW: "Flash%20Flood%20Warning",
+    let longName: [PolygonTypeGeneric: String] = [
+        .SMW: "Special%20Marine%20Warning",
+        .SQW: "Snow%20Squall%20Warning",
+        .DSW: "Dust%20Storm%20Warning",
+        .SPS: "Special%20Weather%20Statement",
+        //PolygonType.TOR: "Tornado%20Warning",
+        //PolygonType.TST: "Severe%20Thunderstorm%20Warning",
+        //PolygonType.FFW: "Flash%20Flood%20Warning",
         //PolygonType.SPS: "Flood%20Warning"
     ]
     
     static let polygonList = [
-        PolygonType.TOR,
-        PolygonType.TST,
-        PolygonType.FFW,
-        PolygonType.SMW,
-        PolygonType.SQW,
-        PolygonType.DSW,
-        PolygonType.SPS
+        //PolygonTypeGeneric.TOR,
+        //PolygonTypeGeneric.TST,
+        //PolygonTypeGeneric.FFW,
+        PolygonTypeGeneric.SMW,
+        PolygonTypeGeneric.SQW,
+        PolygonTypeGeneric.DSW,
+        PolygonTypeGeneric.SPS
     ]
     
-    static var polygonDataByType: [PolygonType: ObjectPolygonWarning] = [:]
+    static var polygonDataByType: [PolygonTypeGeneric: ObjectPolygonWarning] = [:]
     
     static func load() {
         polygonList.forEach{
@@ -64,5 +65,5 @@ class ObjectPolygonWarning {
         }
     }
     
-    let baseUrl = "https://api.weather.gov/alerts/active?event="*/
+    let baseUrl = "https://api.weather.gov/alerts/active?event="
 }
