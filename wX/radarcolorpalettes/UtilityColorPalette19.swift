4bcbc7acf7ef5ddc94793a759d815466ded526b2
Joshua Tee <joshua.tee@gmail.com>
2019-07-16 06:08:02 -0400

    [ADD] all colorpal for 19 - to use with tr0

---
+++ wX/radarcolorpalettes/UtilityColorPalette19.swift
@@ -0,0 +1,71 @@
+/*****************************************************************************
+ * Copyright (c) 2016,2017 joshua.tee@gmail.com. All rights reserved.
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+public class UtilityColorPalette19 {
+
+    class func gen19()
+    {
+        MyApplication.colorMap19R.position(position:0)
+        MyApplication.colorMap19G.position(position:0 )
+        MyApplication.colorMap19B.position(position:0 )
+
+        MyApplication.colorMap19R.put(byte: UInt8(0.0*255.0))
+        MyApplication.colorMap19R.put(byte: UInt8(0.0*255.0))
+        MyApplication.colorMap19R.put(byte: UInt8( 0.003921569*255.0))
+        
+        MyApplication.colorMap19R.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19R.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19R.put(byte: UInt8(0*255.0))
+        
+        MyApplication.colorMap19R.put(byte: UInt8(0*255.0))
+        
+        MyApplication.colorMap19R.put(byte: UInt8(1.0*255.0))
+        MyApplication.colorMap19R.put(byte: UInt8(0.90588236*255.0))
+        MyApplication.colorMap19R.put(byte: UInt8(1.0*255.0))
+        MyApplication.colorMap19R.put(byte: UInt8(1.0*255.0))
+        MyApplication.colorMap19R.put(byte: UInt8(0.8392157*255.0))
+        MyApplication.colorMap19R.put(byte: UInt8(0.7529412*255.0))
+        MyApplication.colorMap19R.put(byte: UInt8(1.0*255.0))
+        MyApplication.colorMap19R.put(byte: UInt8(0.6*255.0))
+        MyApplication.colorMap19R.put(byte: UInt8(1.0*255.0))
+
+        MyApplication.colorMap19G.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19G.put(byte: UInt8(0.9254902*255.0))
+        MyApplication.colorMap19G.put(byte: UInt8(0.627451*255.0))
+        MyApplication.colorMap19G.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19G.put(byte: UInt8(1.0*255.0))
+        MyApplication.colorMap19G.put(byte: UInt8(0.78431374*255.0))
+        MyApplication.colorMap19G.put(byte: UInt8(0.5647059*255.0))
+        MyApplication.colorMap19G.put(byte: UInt8(1.0*255.0))
+        MyApplication.colorMap19G.put(byte: UInt8( 0.7529412*255.0))
+        MyApplication.colorMap19G.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19G.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19G.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19G.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19G.put(byte: UInt8( 0.33333334*255.0))
+        MyApplication.colorMap19G.put(byte: UInt8(1.0*255.0))
+
+       
+        MyApplication.colorMap19B.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19B.put(byte: UInt8( 0.9254902*255.0))
+        MyApplication.colorMap19B.put(byte: UInt8(0.9647059*255.0))
+        MyApplication.colorMap19B.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19B.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19B.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19B.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19B.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19B.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19B.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19B.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19B.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19B.put(byte: UInt8(0*255.0))
+        MyApplication.colorMap19B.put(byte: UInt8(1.0*255.0))
+        MyApplication.colorMap19B.put(byte: UInt8(0.7882353*255.0))
+        MyApplication.colorMap19B.put(byte: UInt8(1.0*255.0))
+    }
+}
+
+
