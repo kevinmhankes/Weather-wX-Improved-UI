edbca85cbd181dea8151816366dfd4bf747f5568
Joshua Tee <joshua.tee@gmail.com>
2020-01-12 15:29:03 -0500

    [ADD] RadarFile class from flutter/dart port (wip)

---
+++ wX/radar/RadarGeometryFileType.swift
@@ -0,0 +1,15 @@
+/*****************************************************************************
+ * Copyright (c) 2016, 2017, 2018, 2019, 2020 joshua.tee@gmail.com. All rights reserved.
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+enum RadarGeometryFileType {
+    case stateLines
+    case countyLines
+    case highways
+    case rivers
+    case highwaysExtended
+    case mexicoLines
+    case canadaLines
+}
