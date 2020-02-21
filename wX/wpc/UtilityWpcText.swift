9c6396431a420b97196da7cd5070357ea7c94dd2
Joshua Tee <joshua.tee@gmail.com>
2020-02-20 19:40:33 -0500

    misc refactor

--- wX/wpc/UtilityWpcText.swift
+++ wX/wpc/UtilityWpcText.swift
@@ -5,9 +5,8 @@
  *****************************************************************************/
 
 final class UtilityWpcText {
-
+    
     static let titles = [
-        //ObjectMenuTitle("General", 3),
         ObjectMenuTitle("General Forecast Discussions", 11),
         ObjectMenuTitle("Precipitation Discussions", 2),
         ObjectMenuTitle("Hazards", 7),
@@ -20,12 +19,12 @@ final class UtilityWpcText {
         ObjectMenuTitle("Canada", 11),
         ObjectMenuTitle("Space Weather", 6)
     ]
-
+    
     static let labels = [
         "pmdspd: Short Range Forecast Discussion",
         "pmdepd: Extended Forecast Discussion",
         "pmdhmd: Model Diagnostics Discussion",
-
+        
         "pmdhi: Hawaii Extended Forecast Discussion",
         "pmdak: Alaska Extended Forecast Discussion",
         "pmdsa: South American Synoptic Discussion",
@@ -34,10 +33,10 @@ final class UtilityWpcText {
         "pmd30d: Prognostic disc for Monthly Outlook",
         "pmd90d: Prognostic disc for long-lead Seasonal Outlooks",
         "pmdhco: Prognostic disc for long-lead Hawaiian Outlooks",
-
+        
         "qpferd: Excessive Rainfall Discussion",
         "qpfhsd: Heavy Snow and Icing Discussion",
-
+        
         "ushzd37: CPC US Hazards Outlook Days 3-7",
         "pmdthr: CPC US Hazards Outlook Days 8-14",
         "sccns1: Storm Summary 1",
@@ -45,7 +44,7 @@ final class UtilityWpcText {
         "sccns3: Storm Summary 3",
         "sccns4: Storm Summary 4",
         "sccns5: Storm Summary 5",
-
+        
         "miahsfat2: High Seas Forecasts - Atlantic",
         "miahsfep2: High Seas Forecasts - NE Pacific",
         "miahsfep3: High Seas Forecasts - SE Pacific",
@@ -84,18 +83,18 @@ final class UtilityWpcText {
         "offn13: Navtex Marine fcst for Kodiak, AK (West)",
         "offn14: Navtex Marine fcst for Kodiak, AK (NW)",
         "offn15: Navtex Marine fcst for Kodiak, AK(Arctic)",
-
+        
         "uvicac: NOAA/EPA Ultraviolet Index /UVI/ Forecast",
         "tptwrn: Hourly temp/wx for Western US",
         "tptcrn: Hourly temp/wx for Central US",
         "tptern: Hourly temp/wx for Eastern US",
         "tptnam: Hourly temp/wx for NA Cities",
-
+        
         "rwrmx: Latin America and Caribbean Regional Weather Roundup",
         "tptcan: Canadian temp and precip Table",
         "tptint: Foreign temp and weather table",
         "tptlat: Latin American temp and weather table",
-
+        
         "swomcd: Most recent MCD",
         "swody1: Day 1 Convective Outlook",
         "swody2: Day 2 Convective Outlook",
@@ -104,14 +103,14 @@ final class UtilityWpcText {
         "fwddy1: Day 1 Fire Weather Outlook",
         "fwddy2: Day 2 Fire Weather Outlook",
         "fwddy38: Days 3-8 Fire Weather Outlook",
-
+        
         "miatwoat: ATL Tropical Weather Outlook",
         "miatwdat: ATL Tropical Weather Discussion",
         "miatwoep: EPAC Tropical Weather Outlook",
         "miatwdep: EPAC Tropical Weather Discussion",
         "miatwsat: ATL Monthly Tropical Summary",
         "miatwsep: EPAC Monthly Tropical Summary",
-
+        
         "GLFLM: Lake Michigan - Open Lake Forecast",
         "GLFLS: Lake Superior - Open Lake Forecast",
         "GLFLH: Lake Huron - Open Lake Forecast",
@@ -119,7 +118,7 @@ final class UtilityWpcText {
         "GLFLE: Lake Erie - Open Lake Forecast",
         "GLFLO: Lake Ontario - Open Lake Forecast",
         "GLFSL: Saint Lawrence River",
-
+        
         "focn45: Significant Weather Discussion, PASPC",
         "fxcn01_d1-3_west: FXCN01 D1-3 WEST",
         "fxcn01_d4-7_west: FXCN01 D4-7 WEST",
@@ -131,7 +130,7 @@ final class UtilityWpcText {
         "awcn14: Weather Summary N. Saskatchewan",
         "awcn15: Weather Summary S. Alberta",
         "awcn16: Weather Summary N. Alberta",
-
+        
         "swpc3day: NOAA Geomagnetic Activity Observation and Forecast",
         "swpc3daygeo: NOAA Geomagnetic Activity Probabilities",
         "swpchigh: Weekly Highlights and Forecasts",
