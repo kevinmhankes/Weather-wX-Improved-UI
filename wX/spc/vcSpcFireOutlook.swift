32f1ba4ddd62c79eabc6b37ac10b0e0ea73863ac
Joshua Tee <joshua.tee@gmail.com>
2020-02-27 17:08:05 -0500

    [ADD] UIwXViewControllerWithAudio for superclass supporting TTS and playlist

--- wX/spc/vcSpcFireOutlook.swift
+++ wX/spc/vcSpcFireOutlook.swift
@@ -5,29 +5,28 @@
  *****************************************************************************/
 
 import UIKit
-import AVFoundation
 
-class vcSpcFireOutlook: UIwXViewController, AVSpeechSynthesizerDelegate {
-
+class vcSpcFireOutlook: UIwXViewControllerWithAudio {
+    
     private var bitmap = Bitmap()
-    private var playListButton = ObjectToolbarIcon()
-    private var playButton = ObjectToolbarIcon()
     private var text = ""
-    private var synth = AVSpeechSynthesizer()
-    private var product = ""
     var dayIndex = 0
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        synth.delegate = self
         let shareButton = ObjectToolbarIcon(self, .share, #selector(shareClicked))
-        playButton = ObjectToolbarIcon(self, .play, #selector(playClicked))
-        playListButton = ObjectToolbarIcon(self, .playList, #selector(playlistClicked))
-        toolbar.items = ObjectToolbarItems([doneButton, GlobalVariables.flexBarButton, playButton, playListButton, shareButton]).items
+        toolbar.items = ObjectToolbarItems(
+            [
+                doneButton,
+                GlobalVariables.flexBarButton,
+                playButton,
+                playListButton,
+                shareButton
+        ]).items
         objScrollStackView = ObjectScrollStackView(self, scrollView, stackView, toolbar)
         self.getContent()
     }
-
+    
     func getContent() {
         DispatchQueue.global(qos: .userInitiated).async {
             let imgUrl = UtilitySpcFireOutlook.urls[self.dayIndex]
@@ -39,36 +38,17 @@ class vcSpcFireOutlook: UIwXViewController, AVSpeechSynthesizerDelegate {
             }
         }
     }
-
-    @objc override func doneClicked() {
-        UtilityActions.resetAudio(&synth, playButton)
-        super.doneClicked()
-    }
-
+    
     @objc func imageClicked(sender: UITapGestureRecognizerWithData) {
         let vc = vcImageViewer()
         vc.imageViewerUrl = UtilitySpcFireOutlook.urls[dayIndex]
         self.goToVC(vc)
     }
-
-    @objc func shareClicked(sender: UIButton) {
+    
+    @objc override func shareClicked(sender: UIButton) {
         UtilityShare.shareImage(self, sender, bitmap, text)
     }
-
-    @objc func playClicked() {
-        UtilityActions.playClicked(text, synth, playButton)
-    }
-
-    @objc func playlistClicked() {
-        UtilityPlayList.add(self.product, text, self, playListButton)
-    }
-
-    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
-        DispatchQueue.main.async {
-            UtilityActions.resetAudio(&self.synth, self.playButton)
-        }
-    }
-
+    
     private func displayContent() {
         var tabletInLandscape = UtilityUI.isTablet() && UtilityUI.isLandscape()
         #if targetEnvironment(macCatalyst)
@@ -97,7 +77,6 @@ class vcSpcFireOutlook: UIwXViewController, AVSpeechSynthesizerDelegate {
         objectImage.img.accessibilityLabel = text
         objectImage.img.isAccessibilityElement = true
         views.append(objectImage.img)
-        let objectTextView: ObjectTextView
         if tabletInLandscape {
             objectTextView = ObjectTextView(self.stackView, self.text, widthDivider: 2)
         } else {
@@ -108,7 +87,7 @@ class vcSpcFireOutlook: UIwXViewController, AVSpeechSynthesizerDelegate {
         self.view.bringSubviewToFront(self.toolbar)
         scrollView.accessibilityElements = views
     }
-
+    
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         coordinator.animate(
