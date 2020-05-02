279b3b6ad0ab231907c5eaf3ef01c4a2610cabba
Joshua Tee <joshua.tee@gmail.com>
2020-05-02 16:56:42 -0400

    [ADD] in ui/CustomTabBarVC remove custom font for tab labels

--- wX/ui/CustomTabBarVC.swift
+++ wX/ui/CustomTabBarVC.swift
@@ -11,31 +11,28 @@ final class CustomTabBarVC: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let appearance = UITabBarItem.appearance()
-        let attributes = [NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-Medium", size: 14)]
-        appearance.setTitleTextAttributes(attributes as Any as? [NSAttributedString.Key: Any], for: .normal)
+        //let appearance = UITabBarItem.appearance()
+        //let attributes = [NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-Medium", size: 14)]
+        //appearance.setTitleTextAttributes(attributes as Any as? [NSAttributedString.Key: Any], for: .normal)
         self.tabBar.tintColor = .white
         self.tabBar.barTintColor = .white
         self.tabBar.isTranslucent = true
         if let items = self.tabBar.items {
             items.forEach {
-                if let image = $0.image {
-                    $0.image = image.withRenderingMode(.alwaysOriginal)
-                }
+                if let image = $0.image { $0.image = image.withRenderingMode(.alwaysOriginal) }
             }
         }
-        
         // To remove dependancy on storyboard uncomment the following and 3 lines in AppDelegate.application
         // In Deployment Info change Main Interface from Main to ""
-        
-        /*let firstViewController = vcTabLocation()
+        /*
+        let firstViewController = vcTabLocation()
         firstViewController.tabBarItem = UITabBarItem(title: "LOCAL", image: nil, tag: 0)
         let secondViewController = vcTabSpc()
         secondViewController.tabBarItem = UITabBarItem(title: "SPC", image: nil, tag: 1)
         let thirdViewController = vcTabMisc()
         thirdViewController.tabBarItem = UITabBarItem(title: "MISC", image: nil, tag: 2)
         let tabBarList = [firstViewController, secondViewController, thirdViewController]
-        viewControllers = tabBarList*/
-        
+        viewControllers = tabBarList
+         */
     }
 }
