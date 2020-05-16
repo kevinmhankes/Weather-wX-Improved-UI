853fdb5ab3d8bf1cc70aa0b08cba131fdfb1a9d9
Joshua Tee <joshua.tee@gmail.com>
2020-05-16 12:30:34 -0400

    [REF] misc

--- wX/models/UtilityModelSpcSrefInputOutput.swift
+++ wX/models/UtilityModelSpcSrefInputOutput.swift
@@ -8,18 +8,18 @@ import UIKit
 
 final class UtilityModelSpcSrefInputOutput {
     
-    static let srefPattern2 = "([0-9]{10}z</a>&nbsp in through <b>f[0-9]{3})"
-    static let srefPattern3 = "<tr><td class=.previous.><a href=sref.php\\?run=[0-9]{10}&id=SREF_H5__>([0-9]{10}z)</a></td></tr>"
+    static let pattern1 = "([0-9]{10}z</a>&nbsp in through <b>f[0-9]{3})"
+    static let pattern2 = "<tr><td class=.previous.><a href=sref.php\\?run=[0-9]{10}&id=SREF_H5__>([0-9]{10}z)</a></td></tr>"
     
     static func getRunTime() -> RunTimeData {
         let runData = RunTimeData()
         let html = (MyApplication.nwsSPCwebsitePrefix + "/exper/sref/").getHtml()
-        var tmpTxt = html.parse(srefPattern2)
-        let results = html.parseColumn(srefPattern3)
+        var tmpTxt = html.parse(pattern1)
+        let results = html.parseColumn(pattern2)
         let latestRun = tmpTxt.split("</a>")[0]
         runData.appendListRun(latestRun.replace("z", ""))
         results.forEach { runData.appendListRun($0.replace("z", "")) }
-        tmpTxt = tmpTxt.parse(srefPattern2).parse("(f[0-9]{3})")
+        tmpTxt = tmpTxt.parse(pattern1).parse("(f[0-9]{3})")
         runData.imageCompleteStr = tmpTxt
         if runData.listRun.count > 0 { runData.mostRecentRun = runData.listRun[0] }
         return runData
