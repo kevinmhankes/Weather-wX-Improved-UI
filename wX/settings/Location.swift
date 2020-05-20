bbb16c190d43bbe7862e53a069ffcb335f5cd71e
Joshua Tee <joshua.tee@gmail.com>
2020-05-20 04:01:55 -0400

    [REF] misc

--- wX/settings/Location.swift
+++ wX/settings/Location.swift
@@ -27,34 +27,34 @@ final class Location {
     var observation: String
     private let prefNumberString: String
 
-    init(_ locNumInt: Int) {
-        let jStr = String(locNumInt + 1)
-        prefNumberString = jStr
-        lat = Utility.readPref("LOC" + jStr + "_X", "")
-        lon = Utility.readPref("LOC" + jStr + "_Y", "")
-        name = Utility.readPref("LOC" + jStr + "_LABEL", "")
-        countyCurrent = Utility.readPref("COUNTY" + jStr, "")
-        zoneCurrent = Utility.readPref("ZONE" + jStr, "")
-        wfo = Utility.readPref("NWS" + jStr, "")
-        rid = Utility.readPref("RID" + jStr, "")
-        nwsStateCurrent = Utility.readPref("NWS" + jStr + "_STATE", "")
+    init(_ locNumAsInt: Int) {
+        let locNumAsString = String(locNumAsInt + 1)
+        prefNumberString = locNumAsString
+        lat = Utility.readPref("LOC" + locNumAsString + "_X", "")
+        lon = Utility.readPref("LOC" + locNumAsString + "_Y", "")
+        name = Utility.readPref("LOC" + locNumAsString + "_LABEL", "")
+        countyCurrent = Utility.readPref("COUNTY" + locNumAsString, "")
+        zoneCurrent = Utility.readPref("ZONE" + locNumAsString, "")
+        wfo = Utility.readPref("NWS" + locNumAsString, "")
+        rid = Utility.readPref("RID" + locNumAsString, "")
+        nwsStateCurrent = Utility.readPref("NWS" + locNumAsString + "_STATE", "")
         state = Utility.getRadarSiteName(rid).split(",")[0]
-        observation = Utility.readPref("LOC" + jStr + "_OBSERVATION", "")
+        observation = Utility.readPref("LOC" + locNumAsString + "_OBSERVATION", "")
         isLocationUS = Location.us(lat)
         Location.addToListOfNames(name)
     }
 
     func saveLocationToNewSlot(_ newLocNumInt: Int) {
-        let iStr = String(newLocNumInt + 1)
-        Utility.writePref("LOC" + iStr + "_X", lat)
-        Utility.writePref("LOC" + iStr + "_Y", lon)
-        Utility.writePref("LOC" + iStr + "_LABEL", name)
-        Utility.writePref("COUNTY" + iStr, countyCurrent)
-        Utility.writePref("ZONE" + iStr, zoneCurrent)
-        Utility.writePref("NWS" + iStr, wfo)
-        Utility.writePref("RID" + iStr, rid)
-        Utility.writePref("NWS" + iStr + "_STATE", nwsStateCurrent)
-        Utility.writePref("LOC" + iStr + "_OBSERVATION", observation)
+        let locNumAsString = String(newLocNumInt + 1)
+        Utility.writePref("LOC" + locNumAsString + "_X", lat)
+        Utility.writePref("LOC" + locNumAsString + "_Y", lon)
+        Utility.writePref("LOC" + locNumAsString + "_LABEL", name)
+        Utility.writePref("COUNTY" + locNumAsString, countyCurrent)
+        Utility.writePref("ZONE" + locNumAsString, zoneCurrent)
+        Utility.writePref("NWS" + locNumAsString, wfo)
+        Utility.writePref("RID" + locNumAsString, rid)
+        Utility.writePref("NWS" + locNumAsString + "_STATE", nwsStateCurrent)
+        Utility.writePref("LOC" + locNumAsString + "_OBSERVATION", observation)
         Location.refreshLocationData()
     }
 
