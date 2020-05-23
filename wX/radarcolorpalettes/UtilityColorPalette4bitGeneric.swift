600af55587aeb487377c8ef59e81fd8eb42f557c
Joshua Tee <joshua.tee@gmail.com>
2020-05-23 09:10:37 -0400

    [REF] misc

--- wX/radarcolorpalettes/UtilityColorPalette4bitGeneric.swift
+++ wX/radarcolorpalettes/UtilityColorPalette4bitGeneric.swift
@@ -7,18 +7,14 @@
 final class UtilityColorPalette4bitGeneric {
 
     static func generate(_ product: Int) {
-        MyApplication.colorMap[product]!.redValues.position = 0
-        MyApplication.colorMap[product]!.greenValues.position = 0
-        MyApplication.colorMap[product]!.blueValues.position = 0
+        MyApplication.colorMap[product]!.position(0)
         UtilityIO.readTextFile("colormap" + String(product) + ".txt").split("\n").forEach { line in
             if line.contains(",") {
                 let colors = line.split(",")
                 let red = UInt8(colors[0])!
                 let green = UInt8(colors[1])!
                 let blue = UInt8(colors[2])!
-                MyApplication.colorMap[product]!.redValues.put(red)
-                MyApplication.colorMap[product]!.greenValues.put(green)
-                MyApplication.colorMap[product]!.blueValues.put(blue)
+                MyApplication.colorMap[product]!.putBytes(red, green, blue)
             }
         }
     }
