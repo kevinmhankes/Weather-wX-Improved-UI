46375b5de77bd2879fde4a3344dc3c2947d8624a
Joshua Tee <joshua.tee@gmail.com>
2020-05-31 16:48:50 -0400

    [REF] misc

--- wX/radar/ObjectMetalRadarBuffers.swift
+++ wX/radar/ObjectMetalRadarBuffers.swift
@@ -5,14 +5,14 @@
  *****************************************************************************/
 
 final class ObjectMetalRadarBuffers: ObjectMetalBuffers {
-
+    
     var bgColor = 0
     var fileName = "nids"
     var rd = WXMetalNexradLevelData()
     let colorRF: Float
     let colorGF: Float
     let colorBF: Float
-
+    
     init(_ bgColor: Int) {
         self.bgColor = bgColor
         colorRF = Float(Color.red(bgColor)) / 255.0
@@ -21,7 +21,7 @@ final class ObjectMetalRadarBuffers: ObjectMetalBuffers {
     }
     
     var colorMap: ObjectColorPalette { MyApplication.colorMap[Int(self.rd.productCode)]! }
-
+    
     func initialize() {
         if rd.productCode == 37 || rd.productCode == 38 || rd.productCode == 41 || rd.productCode == 57 {
             if floatBuffer.capacity < (48 * 464 * 464) { floatBuffer = MemoryBuffer(48 * 464 * 464) }
@@ -30,10 +30,26 @@ final class ObjectMetalRadarBuffers: ObjectMetalBuffers {
         }
         setToPositionZero()
     }
-
+    
     func putColorsByIndex(_ level: UInt8) {
         putColor(colorMap.redValues.get(Int(level)))
         putColor(colorMap.greenValues.get(Int(level)))
         putColor(colorMap.blueValues.get(Int(level)))
+    }
+    
+    func generateRadials() -> Int {
+        let totalBins: Int
+        switch rd.productCode {
+        case 37, 38:
+            totalBins = UtilityWXMetalPerfRaster.generate(self)
+        case 153, 154, 30, 56, 78, 80, 181:
+            totalBins = UtilityWXMetalPerf.genRadials(self)
+        case 0:
+            totalBins = 0
+            break
+        default:
+            totalBins = UtilityWXMetalPerf.decode8BitAndGenRadials(self)
+        }
+        return totalBins
     }
 }
