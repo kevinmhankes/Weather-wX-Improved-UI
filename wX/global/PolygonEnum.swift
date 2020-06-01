7a48510f3f741e2d5e32da187c4faed6eca0d917
Joshua Tee <joshua.tee@gmail.com>
2020-06-01 19:59:58 -0400

    [REF] misc

---
+++ wX/global/PolygonEnum.swift
@@ -0,0 +1,11 @@
+/*****************************************************************************
+ * Copyright (c) 2016, 2017, 2018, 2019, 2020 joshua.tee@gmail.com. All rights reserved.
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+enum PolygonEnum {
+    case SPCMCD
+    case SPCWAT
+    case WPCMCD
+}
