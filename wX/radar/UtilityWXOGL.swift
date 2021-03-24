0a7cd04dff0714b273b774c10a575ccc36dffa32
Joshua Tee <joshua.tee@gmail.com>
2021-03-24 18:25:41 -0400

    [REF] misc

--- wX/radar/UtilityWXOGL.swift
+++ wX/radar/UtilityWXOGL.swift
@@ -6,40 +6,65 @@
 
 final class UtilityWXOGL {
 
+//    static func showTextProducts(_ latLon: LatLon) -> String {
+//        var warningChunk = MyApplication.severeDashboardTor.value + MyApplication.severeDashboardTst.value + MyApplication.severeDashboardFfw.value
+//        ObjectPolygonWarning.polygonList.forEach { poly in
+//            let it = ObjectPolygonWarning.polygonDataByType[poly]!
+//            if it.isEnabled {
+//                warningChunk += it.storage.value
+//            }
+//        }
+//        // var urlList = warningChunk.parseColumn("\"id\"\\: .(https://api.weather.gov/alerts/NWS-IDP-.*?)\"")
+//        var urlList = warningChunk.parseColumn("\"id\"\\: .(https://api.weather.gov/alerts/urn.*?)\"")
+//        let urlListCopy = urlList
+//        // discard  "id": "https://api.weather.gov/alerts/NWS-IDP-PROD-3771044", "type": "Feature", "geometry": null,
+//        // Special Weather Statements can either have a polygon or maybe not, need to strip out those w/o polygon
+//        urlListCopy.forEach { url in
+//            //if (html.contains(Regex("\"id\"\\: ." + it + "\",            \"type\": \"Feature\",            \"geometry\": null"))) {
+//            if warningChunk.matches(regexp: "\"id\"\\: ." + url + "\",\\s*\"type\": \"Feature\",\\s*\"geometry\": null") {
+//                urlList.removeAll { $0 == url }
+//            }
+//        }
+//        warningChunk = warningChunk.replace("\n", "").replace(" ", "")
+//        let polygons = warningChunk.parseColumn("\"coordinates\":\\[\\[(.*?)\\]\\]\\}")
+//        var s = ""
+//        var notFound = true
+//        polygons.enumerated().forEach { urlIndex, warning in
+//            let polygonTmp = warning.replace("[", "").replace("]", "").replace(",", " ")
+//            let latLons = LatLon.parseStringToLatLons(polygonTmp)
+//            if latLons.count > 0 {
+//                let contains = ExternalPolygon.polygonContainsPoint(latLon, latLons)
+//                if contains && notFound {
+//                    s = urlList[urlIndex]
+//                    notFound = false
+//                }
+//            }
+//        }
+//        return s
+//    }
+    
     static func showTextProducts(_ latLon: LatLon) -> String {
         var warningChunk = MyApplication.severeDashboardTor.value + MyApplication.severeDashboardTst.value + MyApplication.severeDashboardFfw.value
-        ObjectPolygonWarning.polygonList.forEach { poly in
-            let it = ObjectPolygonWarning.polygonDataByType[poly]!
+        for data in ObjectPolygonWarning.polygonList {
+            let it = ObjectPolygonWarning.polygonDataByType[data]!
             if it.isEnabled {
                 warningChunk += it.storage.value
             }
         }
-        // var urlList = warningChunk.parseColumn("\"id\"\\: .(https://api.weather.gov/alerts/NWS-IDP-.*?)\"")
-        var urlList = warningChunk.parseColumn("\"id\"\\: .(https://api.weather.gov/alerts/urn.*?)\"")
-        let urlListCopy = urlList
-        // discard  "id": "https://api.weather.gov/alerts/NWS-IDP-PROD-3771044", "type": "Feature", "geometry": null,
-        // Special Weather Statements can either have a polygon or maybe not, need to strip out those w/o polygon
-        urlListCopy.forEach { url in
-            //if (html.contains(Regex("\"id\"\\: ." + it + "\",            \"type\": \"Feature\",            \"geometry\": null"))) {
-            if warningChunk.matches(regexp: "\"id\"\\: ." + url + "\",\\s*\"type\": \"Feature\",\\s*\"geometry\": null") {
-                urlList.removeAll { $0 == url }
-            }
-        }
-        warningChunk = warningChunk.replace("\n", "").replace(" ", "")
-        let polygons = warningChunk.parseColumn("\"coordinates\":\\[\\[(.*?)\\]\\]\\}")
-        var s = ""
+        let warnings = ObjectWarning.parseJson(warningChunk);
+        var urlToOpen = ""
         var notFound = true
-        polygons.enumerated().forEach { urlIndex, warning in
-            let polygonTmp = warning.replace("[", "").replace("]", "").replace(",", " ")
-            let latLons = LatLon.parseStringToLatLons(polygonTmp)
+        for w in warnings {
+            // qDebug() << w.geometry << " " << w.vtec << " " << w.sender;
+            let latLons = w.getPolygonAsLatLons(1)
             if latLons.count > 0 {
                 let contains = ExternalPolygon.polygonContainsPoint(latLon, latLons)
                 if contains && notFound {
-                    s = urlList[urlIndex]
+                    urlToOpen = w.getUrl()
                     notFound = false
                 }
             }
         }
-        return s
+        return urlToOpen
     }
 }
