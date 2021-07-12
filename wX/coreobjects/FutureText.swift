77daa8449ad8622e928dec26ff97c42cc309238a
Joshua Tee <joshua.tee@gmail.com>
2021-07-12 17:03:58 -0400

    [REF] misc

---
+++ wX/coreobjects/FutureText.swift
@@ -0,0 +1,23 @@
+/*****************************************************************************
+ * Copyright (c) 2016, 2017, 2018, 2019, 2020, 2021 joshua.tee@gmail.com. All rights reserved.
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+import Foundation
+
+final class FutureText {
+    
+    private let arg1: String
+    private let updateFunc: (String) -> Void
+
+    init(_ arg1: String, _ updateFunc: @escaping (String) -> Void) {
+        self.arg1 = arg1
+        self.updateFunc = updateFunc
+        DispatchQueue.global(qos: .userInitiated).async {
+            // let s = self.downloadFunc()
+            let s = UtilityDownload.getTextProduct(self.arg1)
+            DispatchQueue.main.async { self.updateFunc(s) }
+        }
+    }
+}
---
+++ wX/coreobjects/FutureVoid.swift
@@ -0,0 +1,22 @@
+/*****************************************************************************
+ * Copyright (c) 2016, 2017, 2018, 2019, 2020, 2021 joshua.tee@gmail.com. All rights reserved.
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+import Foundation
+
+final class FutureVoid {
+    
+    private let downloadFunc: () -> Void
+    private let updateFunc: () -> Void
+
+    init(_ downloadFunc: @escaping () -> Void, _ updateFunc: @escaping () -> Void) {
+        self.downloadFunc = downloadFunc
+        self.updateFunc = updateFunc
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.downloadFunc()
+            DispatchQueue.main.async { self.updateFunc() }
+        }
+    }
+}
