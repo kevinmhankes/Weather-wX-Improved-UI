c4c781393bc7bb3ff1fd424d463d1810ff908f1b
Joshua Tee <joshua.tee@gmail.com>
2021-07-20 05:04:31 -0400

    [REF] misc

--- wX/ui/ObjectSettingsSwitch.swift
+++ wX/ui/ObjectSettingsSwitch.swift
@@ -11,9 +11,11 @@ final class ObjectSettingsSwitch {
     private let button = UIButton(type: UIButton.ButtonType.system)
     let switchUi = UISwitch()
     private let prefMap: [String: String]
+    let prefVar: String
 
     init(_ stackView: UIStackView, _ prefVar: String, _ boolDefArray: [String: String], _ prefMap: [String: String]) {
         self.prefMap = prefMap
+        self.prefVar = prefVar
         button.backgroundColor = ColorCompatibility.systemBackground
         button.setTitleColor(ColorCompatibility.label, for: .normal)
         switchUi.backgroundColor = ColorCompatibility.systemBackground
@@ -39,13 +41,15 @@ final class ObjectSettingsSwitch {
     }
     
     @objc func switchChanged(sender: UISwitch) {
-        let prefLabels = [String](UtilitySettingsUI.boolean.keys).sorted(by: <)
+        // let prefLabels = [String](UtilitySettingsUI.boolean.keys).sorted(by: <)
         let isOnQ = sender.isOn
         var truthString = "false"
         if isOnQ {
             truthString = "true"
         }
-        print("AAA " + truthString + " " + prefLabels[sender.tag])
-        Utility.writePref(prefLabels[sender.tag], truthString)
+        // print("AAA " + truthString + " " + prefLabels[sender.tag])
+        // Utility.writePref(prefLabels[sender.tag], truthString)
+        print("AAA " + prefVar + " " + truthString)
+        Utility.writePref(prefVar, truthString)
     }
 }
