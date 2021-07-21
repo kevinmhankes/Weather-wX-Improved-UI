2e349c48d28edbac3992b66b98accb81ecb7f219
Joshua Tee <joshua.tee@gmail.com>
2021-07-21 19:04:02 -0400

    [REF] misc

--- wX/global/LayoutType.swift
+++
@@ -1,9 +0,0 @@
-/*****************************************************************************
- * Copyright (c) 2016, 2017, 2018, 2019, 2020, 2021 joshua.tee@gmail.com. All rights reserved.
- *
- * Refer to the COPYING file of the official project for license.
- *****************************************************************************/
-
-enum LayoutType {
-    case TAB
-}
