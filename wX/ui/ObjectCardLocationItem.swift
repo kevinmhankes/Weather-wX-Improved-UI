cfece88744ed89d53fc0497507bb6144a33eb14e
Joshua Tee <joshua.tee@gmail.com>
2021-07-24 15:22:48 -0400

    [REF] misc

--- wX/ui/ObjectCardLocationItem.swift
+++ wX/ui/ObjectCardLocationItem.swift
@@ -16,22 +16,22 @@ final class ObjectCardLocationItem {
         _ middleLine: String,
         _ gesture: GestureData
     ) {
-        let objectStackView = ObjectStackView(.fill, .vertical, spacing: 0)
-        let tvName = Text(objectStackView, name, isUserInteractionEnabled: false, isZeroSpacing: false)
-        tvCurrentConditions = Text(objectStackView, observation, isUserInteractionEnabled: false, isZeroSpacing: true)
-        let tvMiddle = Text(objectStackView, middleLine, isUserInteractionEnabled: false, isZeroSpacing: true)
+        let boxV = ObjectStackView(.fill, .vertical, spacing: 0)
+        let tvName = Text(boxV, name, isUserInteractionEnabled: false, isZeroSpacing: false)
+        tvCurrentConditions = Text(boxV, observation, isUserInteractionEnabled: false, isZeroSpacing: true)
+        let tvMiddle = Text(boxV, middleLine, isUserInteractionEnabled: false, isZeroSpacing: true)
         tvName.font = FontSize.large.size
         tvCurrentConditions.font = FontSize.small.size
         tvMiddle.font = FontSize.small.size
         tvName.color = ColorCompatibility.highlightText
         tvCurrentConditions.color = ColorCompatibility.label
         tvMiddle.color = ColorCompatibility.systemGray2
-        uiv.stackView.addLayout(objectStackView.view)
+        uiv.stackView.addLayout(boxV)
         [tvName, tvName, tvCurrentConditions].forEach {
             // $0.tv.widthAnchor.constraint(equalTo: uiv.scrollView.widthAnchor).isActive = true
             $0.constrain(uiv.scrollView)
         }
-        objectStackView.view.addGestureRecognizer(gesture)
-        objectStackView.constrain(uiv.stackView)
+        boxV.addGesture(gesture)
+        boxV.constrain(uiv.stackView)
     }
 }
