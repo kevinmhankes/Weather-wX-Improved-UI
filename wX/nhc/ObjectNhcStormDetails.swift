2216d8828a28f4da2e8d5158b4b2012fb0583faa
Joshua Tee <joshua.tee@gmail.com>
2021-08-07 06:42:31 -0400

    [REF] misc

--- wX/nhc/ObjectNhcStormDetails.swift
+++ wX/nhc/ObjectNhcStormDetails.swift
@@ -60,6 +60,21 @@ final class ObjectNhcStormDetails {
         if modBinNumber.count == 3 {
             modBinNumber = modBinNumber.insert(2, "0")
         }
+
+        // FIXME TODO HFO storm used URL https://www.nhc.noaa.gov/storm_graphics/EP09/EP092021_5day_cone_with_line_and_wind_sm2.png
+//        "activeStorms": [
+//            {
+//                "id": "ep092021",
+//                "binNumber": "CP2",
+//                "name": "Jimena",
+//                "classification": "PTC",
+//                "intensity": "30",
+//                "pressure": "1009",
+//                "latitude": "17.9N",
+//                "longitude": "140.8W",
+//                "latitudeNumeric": 17.9,
+//                "longitudeNumeric": -140.8,
+        
         baseUrl = "https://www.nhc.noaa.gov/storm_graphics/" + modBinNumber + "/" + id.uppercased()
         goesUrl = "https://cdn.star.nesdis.noaa.gov/FLOATER/data/" + id.uppercased() + "/GEOCOLOR/latest.jpg"
     }
