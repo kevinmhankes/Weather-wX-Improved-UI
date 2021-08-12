78bf408ad85d28cd346bb1ade12f849b1cc13917
Joshua Tee <joshua.tee@gmail.com>
2021-08-12 04:50:44 -0400

    [REF] misc

---
+++ wX/radar/RadarGeometryFile.swift
@@ -0,0 +1,15 @@
+// *****************************************************************************
+// Copyright (c)  2016, 2017, 2018, 2019, 2020, 2021 joshua.tee@gmail.com. All rights reserved.
+//
+// Refer to the COPYING file of the official project for license.
+// *****************************************************************************
+
+final class RadarGeometryFile {
+    
+    let type: FrontTypeEnum
+    var coordinates = [LatLon]()
+
+    init(_ type: FrontTypeEnum) {
+        self.type = type
+    }
+}
